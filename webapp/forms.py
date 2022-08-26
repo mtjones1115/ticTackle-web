264a58e57ca3c4b0eb498996dc5d25a349dd72ea
Marquan Jones <mtjones1115@gmail.com>
2022-08-26 17:29:38 -0500

    Added login form and redirection

---
+++ webapp/forms.py
@@ -0,0 +1,11 @@
+from logging.config import valid_ident
+from wsgiref.validate import validator
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
